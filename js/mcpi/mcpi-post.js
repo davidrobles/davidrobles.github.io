fdc291e67d01554c47be9c39496fb6d6b265bbd5
David Robles <drobles@gmail.com>
2014-06-22 11:14:23 -0700

    Call short and full demos from window.onload

--- js/mcpi/mcpi-post.js
+++ js/mcpi/mcpi-post.js
@@ -1,68 +1,82 @@
 (function() {
-    var model = new MCPI.Model({
-        sampleSize: 75000,
-        stepSize: 500
-    });
-    var controller = new MCPI.Controller({
-        model: model
-    });
-    var canvasView = new MCPI.CanvasView({
-        canvasEl: document.getElementById("mcpiShortDemo"),
-        size: 220,
-        pointSize: 1,
-        colors: {
-            bg: "#F2D6B3",      // light brown
-            circle: "#D9B89C",  // brown
-            inside: "#2980b9",  // blue
-            outside: "#c0392b"  // red
-        }
-    });
-    model.bind(canvasView);
-    controller.loop();
-}());
 
-(function() {
+    var initShortDemo = function() {
 
-    var model = new MCPI.Model({
-        sampleSize: 5000,
-        stepSize: 500
-    });
+        var model = new MCPI.Model({
+            sampleSize: 75000,
+            stepSize: 500
+        });
 
-    var controller = new MCPI.Controller({
-        model: model
-    });
+        var controller = new MCPI.Controller({
+            model: model
+        });
 
-    var canvasView = new MCPI.CanvasView({
-        canvasEl: document.getElementById("mcpiCanvasView"),
-        size: 300,
-        pointSize: 1,
-        colors: {
-            bg: "#F2D6B3",      // light brown
-            circle: "#D9B89C",  // brown
-            inside: "#2980b9",  // blue
-            outside: "#c0392b"  // red
-        }
-    });
+        var canvasView = new MCPI.CanvasView({
+            canvasEl: document.getElementById("mcpiShortDemo"),
+            size: 220,
+            pointSize: 1,
+            colors: {
+                bg: "#F2D6B3",      // light brown
+                circle: "#D9B89C",  // brown
+                inside: "#2980b9",  // blue
+                outside: "#c0392b"  // red
+            }
+        });
 
-    var dashboardView = new MCPI.DashboardView({
-        model: model,
-        controller: controller,
-        colors: {
-            inside: "#2980b9",  // blue
-            outside: "#c0392b"  // red
-        },
-        elems: {
-            equation: document.getElementById("mcpiEquation"),
-            insideCounter: document.getElementById("mcpiInsideCounter"),
-            outsideCounter: document.getElementById("mcpiOutsideCounter"),
-            meter: document.getElementById("mcpiDemoMeter"),
-            sampleSize: document.getElementById("mcpiSampleSize"),
-            startButton: document.getElementById("mcpiStartButton")
-        }
-    });
+        model.bind(canvasView);
+        // controller.loop();
+        
+    };
 
-    model.bind(canvasView);
-    model.bind(dashboardView);
-    controller.bind(dashboardView);
-    
+    var initFullDemo = function() {
+
+        var model = new MCPI.Model({
+            sampleSize: 5000,
+            stepSize: 500
+        });
+
+        var controller = new MCPI.Controller({
+            model: model
+        });
+
+        var canvasView = new MCPI.CanvasView({
+            canvasEl: document.getElementById("mcpiCanvasView"),
+            size: 300,
+            pointSize: 1,
+            colors: {
+                bg: "#F2D6B3",      // light brown
+                circle: "#D9B89C",  // brown
+                inside: "#2980b9",  // blue
+                outside: "#c0392b"  // red
+            }
+        });
+
+        var dashboardView = new MCPI.DashboardView({
+            model: model,
+            controller: controller,
+            colors: {
+                inside: "#2980b9",  // blue
+                outside: "#c0392b"  // red
+            },
+            elems: {
+                equation: document.getElementById("mcpiEquation"),
+                insideCounter: document.getElementById("mcpiInsideCounter"),
+                outsideCounter: document.getElementById("mcpiOutsideCounter"),
+                meter: document.getElementById("mcpiDemoMeter"),
+                sampleSize: document.getElementById("mcpiSampleSize"),
+                startButton: document.getElementById("mcpiStartButton")
+            }
+        });
+
+        model.bind(canvasView);
+        model.bind(dashboardView);
+        controller.bind(dashboardView);
+        
+    };
+
+    window.onload = function() {
+        initShortDemo();
+        initFullDemo();
+    };
+
 }());
