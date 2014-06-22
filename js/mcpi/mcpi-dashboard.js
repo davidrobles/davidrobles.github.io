861cff962c03d7f0e4c38b3ee3a758c50f7f7461
David Robles <drobles@gmail.com>
2014-06-21 22:32:04 -0700

    fix issues with elems

--- js/mcpi/mcpi-dashboard.js
+++ js/mcpi/mcpi-dashboard.js
@@ -7,10 +7,12 @@ MCPI.DashboardView = function(options) {
         inside: options.elems.insideCounter,
         outside: options.elems.outsideCounter
     };
-    this.meter = options.elems.meter;
-    this.equation = options.elems.equation;
-    this.sampleSizeEl = options.elems.sampleSize;
-    this.startButton = options.elems.startButton;
+    this.elems = {
+        meter: options.elems.meter,
+        equation: options.elems.equation,
+        sampleSize: options.elems.sampleSize,
+        startButton: options.elems.startButton
+    };
     this.addListeners();
 };
 
@@ -24,23 +26,23 @@ MCPI.DashboardView.prototype = {
     },
 
     addSampleSizeListener: function() {
-        this.sampleSizeEl.addEventListener("change", function(event) {
+        this.elems.sampleSize.addEventListener("change", function(event) {
             this.updateSampleSize();
         }.bind(this));
     },
 
     addStartResetListener: function() {
-        this.startButton.addEventListener("click", function() {
-            if (this.startButton.value === "start") {
+        this.elems.startButton.addEventListener("click", function() {
+            if (this.elems.startButton.value === "start") {
                 this.controller.startAnimation();
-            } else if (this.startButton.value === "stop") {
+            } else if (this.elems.startButton.value === "stop") {
                 this.controller.reset();
             }
         }.bind(this));
     },
 
     updateSampleSize: function() {
-        this.model.sampleSize = parseInt(this.sampleSizeEl.value, 10);
+        this.model.sampleSize = parseInt(this.elems.sampleSize.value, 10);
         this.controller.stepSize = this.model.sampleSize / 100;
     },
 
@@ -62,17 +64,17 @@ MCPI.DashboardView.prototype = {
         this.renderEquation();
         this.renderCounters();
         this.renderMeter();
-        this.startButton.className = "mcpiStartStop mcpiStart";
-        this.startButton.innerHTML = "START";
-        this.startButton.value = "start";
-        this.sampleSizeEl.disabled = false;
+        this.elems.startButton.className = "mcpiStartStop mcpiStart";
+        this.elems.startButton.innerHTML = "START";
+        this.elems.startButton.value = "start";
+        this.elems.sampleSize.disabled = false;
     },
 
     start: function() {
-        this.sampleSizeEl.disabled = true;
-        this.startButton.className = "mcpiStartStop mcpiStop";
-        this.startButton.innerHTML = "RESET";
-        this.startButton.value = "stop";
+        this.elems.sampleSize.disabled = true;
+        this.elems.startButton.className = "mcpiStartStop mcpiStop";
+        this.elems.startButton.innerHTML = "RESET";
+        this.elems.startButton.value = "stop";
     },
 
     // Rendering
@@ -81,7 +83,7 @@ MCPI.DashboardView.prototype = {
         var numPoints = this.model.counters.total,
             sampleSize = this.model.sampleSize,
             completionPercentage = Math.round((numPoints / sampleSize) * 100);
-        this.meter.setAttribute("value", "" + completionPercentage);
+        this.elems.meter.setAttribute("value", "" + completionPercentage);
     },
 
     renderCounters: function() {
@@ -92,12 +94,12 @@ MCPI.DashboardView.prototype = {
     renderEquation: function() {
         var pi = this.model.calculatePi();
         if (this.model.counters.total == 0) {
-            var math = MathJax.Hub.getAllJax(this.equation.id)[0];
+            var math = MathJax.Hub.getAllJax(this.elems.equation.id)[0];
             MathJax.Hub.Queue(["Text",math,"\\pi \\approx 4 \\frac{" +
                 "A_C}{A_S}"]);
 
         } else if (this.model.counters.total % 50 == 0) {
-            var math = MathJax.Hub.getAllJax(this.equation.id)[0];
+            var math = MathJax.Hub.getAllJax(this.elems.equation.id)[0];
             MathJax.Hub.Queue(["Text",math,"\\pi \\approx 4 \\frac{"
                 + this.model.counters.inside + "}{" + this.model.counters.total
                 + "} = " + pi.toFixed(4)]);
