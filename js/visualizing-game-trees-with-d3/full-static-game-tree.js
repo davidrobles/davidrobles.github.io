b22a7aff6752eecded7c129c101d4b758d1f29ca
David Robles <drobles@gmail.com>
2015-01-19 18:06:17 -0800

    Update post

--- js/visualizing-game-trees-with-d3/full-static-game-tree.js
+++ js/visualizing-game-trees-with-d3/full-static-game-tree.js
@@ -1,4 +1,4 @@
-var generateSVGTree = function(tic, mysel, options) {
+var generateSVGTree = function(ticTacToe, svgElem, options) {
 
     var margin = { top: 50, right: 30, bottom: 60, left: 30 },
         width = (options.width || 675) - margin.left - margin.right,
@@ -7,11 +7,11 @@ var generateSVGTree = function(tic, mysel, options) {
         edgeWidth = options.edgeWidth || 2;
 
     var root = {
-        game: tic
+        game: ticTacToe
     };
 
     var svgView = new ma.views.TicTacToeSVG({
-        model: tic,
+        model: ticTacToe,
         sideLength: nodeSize
     });
 
@@ -31,7 +31,7 @@ var generateSVGTree = function(tic, mysel, options) {
 
     var diagonal = d3.svg.diagonal().projection(function(d) { return [d.x, d.y]; });
 
-    var svg = d3.select(mysel)
+    var svg = d3.select(svgElem)
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -47,44 +47,39 @@ var generateSVGTree = function(tic, mysel, options) {
 
     var nodes = tree(root);
 
-    var drawNodes = function() {
+    var nodeMargin = nodeSize / 3.4;
 
-        var nodeMargin = nodeSize / 3.4;
+    svg.selectAll("path")
+        .data(tree.links(nodes))
+        .enter().append("path")
+        .attr("d", diagonal)
+        .attr("fill", "none")
+        .attr("stroke", "#666666")
+        .attr("stroke-width", edgeWidth);
 
-        svg.selectAll("path")
-            .data(tree.links(nodes))
-            .enter().append("path")
-            .attr("d", diagonal)
-            .attr("fill", "none")
-            .attr("stroke", "#666666")
-            .attr("stroke-width", edgeWidth);
+    svg.selectAll("g.node-group")
+        .data(nodes)
+        .enter()
+        .append("g")
+        .attr("class", "node-group")
+        .attr("transform", function(d) {
+            return "translate(" + (d.x - nodeMargin) + ", " + (d.y - nodeMargin) + ")"
+        });
 
-        svg.selectAll("g.node-group")
-            .data(nodes)
-            .enter()
-            .append("g")
-            .attr("class", "node-group")
-            .attr("transform", function(d) {
-                return "translate(" + (d.x - nodeMargin) + ", " + (d.y - nodeMargin) + ")"
-            });
+    // Draw nodes
+    svg.selectAll(".node-group")
+        .each(function(node) {
+            svgView.model = node.game;
+            svgView.svg = d3.select(this);
+            svgView.render();
+        });
 
-        // Draw nodes
-        svg.selectAll(".node-group")
-            .each(function(node) {
-                svgView.model = node.game;
-                svgView.svg = d3.select(this);
-                svgView.render();
-            });
+    // Resize game nodes
+    svg.selectAll(".node-group")
+        .attr("transform", function() {
+            return this.getAttribute("transform") + " scale(0.6)";
+        });
 
-        // Resize game nodes
-        svg.selectAll(".node-group")
-            .attr("transform", function() {
-                return this.getAttribute("transform") + " scale(0.6)";
-            });
-
-    };
-
-    drawNodes();
     return svg;
 };
 
